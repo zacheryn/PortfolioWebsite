a8a76d4002cd5fa6b299d24efdf0d6d739dfec1b
Zachery Nagy <zacheryn@umich.edu>
2024-08-27 01:17:17 -0400

    Added title and description using Helmet

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 import './index.css';
 import Portfolio from './Portfolio';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,13 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Portfolio />
+    <HelmetProvider>
+      <Helmet>
+        <title>Zachery Nagy</title>
+        <meta name="description" content="Zachery Nagy portfolio."/>
+      </Helmet>
+      <Portfolio />
+    </HelmetProvider>
   </React.StrictMode>
 );
 
