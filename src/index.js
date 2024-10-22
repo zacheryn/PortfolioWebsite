ffa4ec3fb19272befe49b1c1c9ad22d6020a2d80
Zachery Nagy <zacheryn@umich.edu>
2024-10-22 11:37:50 -0400

    Fixed meta info

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HelmetProvider, Helmet } from 'react-helmet-async';
 import './index.css';
 import Portfolio from './Portfolio';
 import reportWebVitals from './reportWebVitals';
@@ -8,13 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
-      <Helmet>
-        <title>Zachery Nagy</title>
-        <meta name="description" content="Zachery Nagy portfolio."/>
-      </Helmet>
-      <Portfolio />
-    </HelmetProvider>
+    <Portfolio />
   </React.StrictMode>
 );
 
